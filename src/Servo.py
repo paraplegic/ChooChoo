12aaba0d39ba9518c6f3878b6127ee083e5e5e6b
Rob Sciuk <rob@controlq.com>
2019-08-26 00:32:07 -0400

    servo tests conducted

--- src/Servo.py
+++ src/Servo.py
@@ -40,11 +40,8 @@ pwm.set_pwm_freq(60)
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 while True:
     # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
-    pwm.set_pwm(4, 0, servo_min)
-    pwm.set_pwm(8, 0, servo_min)
-    time.sleep(1)
-    pwm.set_pwm(0, 0, servo_max)
-    pwm.set_pwm(4, 0, servo_max)
-    pwm.set_pwm(8, 0, servo_max)
-    time.sleep(1)
+    for pulse in [ servo_min, int( servo_max/3 ), int( servo_max/2 ), servo_max ]:
+      time.sleep( .3 )
+      for channel in [ 0, 4, 8 ]:
+         pwm.set_pwm(channel, 0, pulse)
+         pwm.set_pwm(channel, 0, pulse)
